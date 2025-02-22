5b05414106d8abcc4d876e27809642947875d28f
Caio Satel <caio_satel@hotmail.com>
2025-02-22 19:36:59 -0300

    feat: implementando regra para verificação do token ao fazer login

--- src/main/java/TrackHours/API/controllers/AuthenticationController.java
+++ src/main/java/TrackHours/API/controllers/AuthenticationController.java
@@ -1,4 +1,57 @@
 package TrackHours.API.controllers;
 
+import TrackHours.API.DTO.User.AuthenticationDTO;
+import TrackHours.API.DTO.User.LoginResponseDTO;
+import TrackHours.API.DTO.User.RegisterDTO;
+import TrackHours.API.entities.User;
+import TrackHours.API.repositories.UserRepository;
+import TrackHours.API.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
 public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody @Validated AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User)auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Object> register(@RequestBody @Validated RegisterDTO data) {
+        if (this.userRepository.findByEmail(data.email()) != null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.name(), data.email(), encryptedPassword, data.role());
+
+        this.userRepository.save(newUser);
+
+        return ResponseEntity.ok(newUser);
+    }
 }
