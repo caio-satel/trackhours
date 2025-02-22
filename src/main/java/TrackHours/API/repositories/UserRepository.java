50f177f8d7423ce410e41775afe5336349f33c4a
Caio Satel <caio_satel@hotmail.com>
2025-02-22 19:30:50 -0300

    feat: criando e implementando repository com método de retornar um UserDetails por email

--- src/main/java/TrackHours/API/repositories/UserRepository.java
+++ src/main/java/TrackHours/API/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package TrackHours.API.repositories;
 
-public class UserRepository {
+import TrackHours.API.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByEmail(String email);
 }
