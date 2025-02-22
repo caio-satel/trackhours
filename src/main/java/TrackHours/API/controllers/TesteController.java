11b885d7ead75e09119007a03759c407cbf32677
Caio Satel <caio_satel@hotmail.com>
2025-02-22 19:29:13 -0300

    feat: criando e implementando controller de teste

--- src/main/java/TrackHours/API/controllers/TesteController.java
+++ src/main/java/TrackHours/API/controllers/TesteController.java
@@ -1,4 +1,15 @@
 package TrackHours.API.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
 public class TesteController {
+
+    @GetMapping
+    public String getTeste() {
+        return "Teste";
+    }
 }
