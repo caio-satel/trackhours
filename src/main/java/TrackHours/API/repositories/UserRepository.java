d3efeeab48f54f86b598d3f6cc9c18f5d43b91ce
Caio Satel <caio_satel@hotmail.com>
2025-02-24 16:39:47 -0300

    feat: adicionando notação @Repository

--- src/main/java/TrackHours/API/repositories/UserRepository.java
+++ src/main/java/TrackHours/API/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package TrackHours.API.repositories;
 import TrackHours.API.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     UserDetails findByEmail(String email);
