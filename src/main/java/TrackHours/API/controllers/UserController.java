52c760d452290dabe0eb3f10a0210061a7565f65
Caio Satel <caio_satel@hotmail.com>
2025-02-24 16:41:00 -0300

    feat: implementando v1 do CRUD de usuários

--- src/main/java/TrackHours/API/controllers/UserController.java
+++ src/main/java/TrackHours/API/controllers/UserController.java
@@ -1,4 +1,65 @@
 package TrackHours.API.controllers;
 
+import TrackHours.API.DTO.User.CreateUserDTO;
+import TrackHours.API.DTO.User.UpdateUserDTO;
+import TrackHours.API.entities.User;
+import TrackHours.API.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDTO createUserDTO) {
+        var userId = userService.createUser(createUserDTO);
+        return ResponseEntity.created(URI.create("/users/" + userId.toString())).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers() {
+        List<User> listUsers = userService.listUsers();
+        return ResponseEntity.ok().body(listUsers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable Long id) {
+        var userById = userService.findUserById(id);
+
+        if (userById.isPresent()) {
+            return ResponseEntity.ok(userById.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> uptadeUserById(@PathVariable Long id, @RequestBody UpdateUserDTO updateUserDTO) {
+        boolean userUpdated = userService.updateUserById(id, updateUserDTO);
+
+        if (!userUpdated) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        boolean userExists = userService.deleteById(id);
+
+        if (!userExists) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/TrackHours/API/services/UserService.java
+++ src/main/java/TrackHours/API/services/UserService.java
@@ -1,4 +1,77 @@
 package TrackHours.API.services;
 
+import TrackHours.API.DTO.User.CreateUserDTO;
+import TrackHours.API.DTO.User.UpdateUserDTO;
+import TrackHours.API.entities.User;
+import TrackHours.API.enumTypes.roles.UserRole;
+import TrackHours.API.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    // Create User
+    public Long createUser(CreateUserDTO createUserDTO) {
+        // Cryptographic password
+        var hashedPassword = passwordEncoder.encode(createUserDTO.password());
+        // DTO -> Entity
+        var userCreated = new User(createUserDTO.name(), createUserDTO.email(), hashedPassword, UserRole.USER);
+        var userSaved = userRepository.save(userCreated);
+
+        return userSaved.getId();
+    }
+
+    // Find All User
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
+    // Find By ID
+    public Optional<User> findUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    // Update User By ID
+    public boolean updateUserById(Long id, UpdateUserDTO updateUserDto) {
+        var userEntity = userRepository.findById(id);
+
+        if(userEntity.isPresent()) {
+            var user = userEntity.get();
+
+            if (updateUserDto.name() != null && updateUserDto.email() != null && updateUserDto.password() != null) {
+                user.setName(updateUserDto.name());
+                user.setEmail(updateUserDto.email());
+
+                //Transforma a senha em HASH antes de salvar
+                String hashedPassword = passwordEncoder.encode(updateUserDto.password());
+                user.setPassword(hashedPassword);
+
+                userRepository.save(user);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Delete By ID
+    public boolean deleteById(Long id) {
+        var userExists = userRepository.existsById(id);
+
+        if(userExists) {
+            userRepository.deleteById(id);
+        }
+
+        return userExists;
+    }
 }
