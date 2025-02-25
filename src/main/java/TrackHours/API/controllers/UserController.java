5a3a5cf43260dc6f00784253449eda795834eaef
Caio Satel <caio_satel@hotmail.com>
2025-02-25 20:33:45 -0300

    feat: melhorias no endpoint de listagem de usuários (utilizando mapper), feat: criando novo endpoint de buscar nome do usuário logado

--- src/main/java/TrackHours/API/controllers/UserController.java
+++ src/main/java/TrackHours/API/controllers/UserController.java
@@ -2,18 +2,25 @@ package TrackHours.API.controllers;
 
 import TrackHours.API.DTO.User.CreateUserDTO;
 import TrackHours.API.DTO.User.UpdateUserDTO;
+import TrackHours.API.DTO.User.UserNoProjectsResponseDTO;
+import TrackHours.API.DTO.mapper.UserMapper;
 import TrackHours.API.entities.User;
 import TrackHours.API.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserMapper map;
 
     @Autowired
     private UserService userService;
@@ -25,9 +32,15 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> listUsers() {
+    public ResponseEntity<List<UserNoProjectsResponseDTO>> listUsers() {
         List<User> listUsers = userService.listUsers();
-        return ResponseEntity.ok().body(listUsers);
+
+        List<UserNoProjectsResponseDTO> usersResponse = listUsers
+                .stream()
+                .map(allUsers -> map.userDtoToUser(allUsers))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(usersResponse);
     }
 
     @GetMapping("/{id}")
@@ -39,6 +52,16 @@ public class UserController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/userLogged")
+    public ResponseEntity<Object> getLoggedUser(Authentication authentication) {
+        String email = authentication.getName(); // Email do usuário autenticado
+        User user = (User) userService.findByEmail(email); // Busca no banco pelo email
+
+        String nameUserLogged = user.getName();
+
+        return ResponseEntity.ok().body(nameUserLogged);
     }
 
     @PutMapping("/{id}")
