fc9842aaaf876f12bc3d2cae59ca17cb97fe967c
Caio Satel <caio_satel@hotmail.com>
2025-03-03 14:52:38 -0300

    feat: melhorias na entidade de release, criação de construtor personalizado e adição de um novo campo

--- src/main/java/TrackHours/API/entities/Release.java
+++ src/main/java/TrackHours/API/entities/Release.java
@@ -2,13 +2,20 @@ package TrackHours.API.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Entity
 @Table(name = "lançamentos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Release {
 
     @Id
@@ -18,17 +25,21 @@ public class Release {
     @Column(nullable = false)
     private String description;
 
-    @Column(name = "data_inicio", nullable = false)
-    @JsonFormat(pattern = "dd/MM/yyy HH:mm")
-    private LocalDateTime startDate;
+    @Column(name = "data_lançamento", nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dateRelease;
 
-    @Column(name = "data_fim", nullable = false)
-    @JsonFormat(pattern = "dd/MM/yyy HH:mm")
-    private LocalDateTime endDate;
+    @Column(name = "horario_inicio", nullable = false)
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime startTime;
+
+    @Column(name = "horario_fim", nullable = false)
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime endTime;
 
     @Column(name = "data_registro", nullable = false, updatable = false)
     @CreationTimestamp
-    @JsonFormat(pattern = "dd/MM/yyy HH:mm")
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime createdAt;
 
     @ManyToOne
@@ -38,4 +49,18 @@ public class Release {
     @ManyToOne
     @JoinColumn(name = "id_usuario", nullable = false)
     private User user;
+
+    public Release(String description,
+                   LocalDate dateRelease,
+                   LocalTime startTime,
+                   LocalTime endTime,
+                   Task task,
+                   User user) {
+        this.description = description;
+        this.dateRelease = dateRelease;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.task = task;
+        this.user = user;
+    }
 }
