7b8df1e4ccd68879a001c8f7f606c13a7de79ae8
Caio Satel <caio_satel@hotmail.com>
2025-03-06 00:15:17 -0300

    refactor: adicionando JsonFormat para datas no DTO de resposta de projetos

--- src/main/java/TrackHours/API/DTO/Project/ProjectResponseDTO.java
+++ src/main/java/TrackHours/API/DTO/Project/ProjectResponseDTO.java
@@ -4,12 +4,15 @@ import TrackHours.API.DTO.User.UserNoProjectsResponseDTO;
 import TrackHours.API.entities.User;
 import TrackHours.API.enumTypes.projects.PriorityProject;
 import TrackHours.API.enumTypes.projects.StatusProject;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDate;
 
 public record ProjectResponseDTO (Long id,
                                   String name,
+                                  @JsonFormat(pattern = "dd/MM/yyyy")
                                   LocalDate startDate,
+                                  @JsonFormat(pattern = "dd/MM/yyyy")
                                   LocalDate endDate,
                                   StatusProject status,
                                   PriorityProject priority,
