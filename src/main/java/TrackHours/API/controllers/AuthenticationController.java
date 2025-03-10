5c863c1bd76369485e2f8f71730bce22aa9e5d33
Caio Satel <caio_satel@hotmail.com>
2025-03-10 10:09:05 -0300

    feat: melhorias nas validações do endpoint de login e exclusão do endpoint de registro

--- src/main/java/TrackHours/API/controllers/AuthenticationController.java
+++ src/main/java/TrackHours/API/controllers/AuthenticationController.java
@@ -2,7 +2,6 @@ package TrackHours.API.controllers;
 
 import TrackHours.API.DTO.User.AuthenticationDTO;
 import TrackHours.API.DTO.User.LoginResponseDTO;
-import TrackHours.API.DTO.User.RegisterDTO;
 import TrackHours.API.entities.User;
 import TrackHours.API.enumTypes.roles.UserRole;
 import TrackHours.API.repositories.UserRepository;
@@ -11,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.annotation.Validated;
@@ -38,39 +38,22 @@ public class AuthenticationController {
     private TokenService tokenService;
 
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody @Validated AuthenticationDTO data){
-        var user = userRepository.findByEmail(data.email());
+    public ResponseEntity<Object> login(@RequestBody @Validated AuthenticationDTO data) {
+        try {
+            var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+            var auth = authenticationManager.authenticate(usernamePassword);
 
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+            var userAuthenticate = (User) auth.getPrincipal();
+
+            // Atualiza o lastLogin com o horário UTC corrigido no banco
+            userAuthenticate.setLastLogin(Instant.now().plus(Duration.ofHours(-3)));
+            userRepository.save(userAuthenticate);
+
+            var token = tokenService.generateToken(userAuthenticate);
+            return ResponseEntity.ok(new LoginResponseDTO(token));
+
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("E-mail ou senha inválidos");
         }
-
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
-        var auth = authenticationManager.authenticate(usernamePassword);
-
-        // Obtem usuário autenticado
-        var userAuthenticate = (User) auth.getPrincipal();
-
-        // Atualiza o lastLogin com UTC correto no banco
-        userAuthenticate.setLastLogin(Instant.now().plus(Duration.ofHours(-3)));
-        userRepository.save(userAuthenticate);
-
-        // Gerar o token
-        var token = tokenService.generateToken(userAuthenticate);
-        return ResponseEntity.ok(new LoginResponseDTO(token));
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<Map<String, String>> register(@RequestBody @Validated RegisterDTO data) {
-        if (this.userRepository.findByEmail(data.email()) != null) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        User newUser = new User(data.name(), data.email(), encryptedPassword, UserRole.ADMIN);
-
-        this.userRepository.save(newUser);
-
-        return ResponseEntity.ok(Map.of("message", "Usuário criado com sucesso!"));
     }
 }
