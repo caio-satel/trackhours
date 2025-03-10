e5bc197ee427258cbca67954d920912e088e6ee6
Caio Satel <caio_satel@hotmail.com>
2025-03-10 20:04:36 -0300

    feat: melhoria nas permições de requisições com base no perfil do usuário

--- src/main/java/TrackHours/API/security/SecurityConfig.java
+++ src/main/java/TrackHours/API/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -36,17 +37,28 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/v3/api-docs/**",       // Endpoints da documentação OpenAPI
+                                "/swagger-ui/**",        // Recursos estáticos do Swagger UI
+                                "/swagger-ui.html"       // Página principal do Swagger UI
+                        ).permitAll()
+                        .requestMatchers("/users/userLogged").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.PATCH,"/users/change-password").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/tasks/byUser").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/releases/byUser").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/releases/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/releases/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/releases/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/dashboard").authenticated()
-                        .requestMatchers("/users").hasRole("ADMIN")
-                        .requestMatchers("/projects").hasRole("ADMIN")
-                        .requestMatchers("/tasks").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/releases").authenticated()
-                        .requestMatchers(HttpMethod.PUT, "/releases/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/releases/**").authenticated()
+                        .requestMatchers("/users/**").hasRole("ADMIN")
+                        .requestMatchers("/projects/**").hasRole("ADMIN")
+                        .requestMatchers("/tasks/**").hasRole("ADMIN")
+                        .requestMatchers("/releases/**").hasRole("ADMIN")
+
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
