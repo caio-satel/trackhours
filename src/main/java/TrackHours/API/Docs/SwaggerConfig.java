e694cd844723305573700d2c4ec4a82197f2aca9
Caio Satel <caio_satel@hotmail.com>
2025-03-10 20:02:55 -0300

    feat: criando e implementando configuração do swagger para documentação

--- src/main/java/TrackHours/API/Docs/SwaggerConfig.java
+++ src/main/java/TrackHours/API/Docs/SwaggerConfig.java
@@ -1,4 +1,29 @@
 package TrackHours.API.Docs;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Documentação API - Trackhours").version("v1").description("Segundo projeto do Trainee da Wise System - Sistema de gerenciamento de projeto/tarefas e lançamento de horas"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER)
+                        .name("Authorization")));
+    }
 }
+
