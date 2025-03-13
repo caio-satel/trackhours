68359a748db85aafa243201c3b352b61d931c8b0
Caio Satel <caio_satel@hotmail.com>
2025-03-13 12:03:16 -0300

    refactor: retirando imports não utilizados

--- src/main/java/TrackHours/API/security/SecurityConfig.java
+++ src/main/java/TrackHours/API/security/SecurityConfig.java
@@ -1,8 +1,5 @@
 package TrackHours.API.security;
 
-import TrackHours.API.entities.User;
-import TrackHours.API.repositories.UserRepository;
-import TrackHours.API.services.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
